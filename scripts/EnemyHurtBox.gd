5d329e92e302e7d23718d8f8acdbc435b2658afe
Jer <jeremiej19@wp.pl>
2025-04-07 18:38:49 +0200

    rewiev

--- scripts/EnemyHurtBox.gd
+++ scripts/EnemyHurtBox.gd
@@ -1,16 +1,11 @@
 class_name EnemyHurtBox
 extends Area2D
 
-func _init() -> void:
-	collision_layer = 0
-	collision_mask = 2
 
 func _ready() -> void:
 	connect("area_entered", _on_area_entered)
 
 func _on_area_entered(hitbox: MyHitBox) -> void:
-	if hitbox == null:
-		return
 	if hitbox == null:
 		return
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -87,3 +87,8 @@ func _physics_process(delta: float) -> void:
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
 			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if "attack" in anim_name:
+		attack_initiated = false
