6d2aea29baadaffba9174e6f177bd842aefa8669
Suselkowy <jakubsus77@gmail.com>
2025-04-08 17:04:17 +0200

    update animations, reverse attack logic

--- scripts/warrior.gd
+++ scripts/warrior.gd
@@ -6,42 +6,33 @@ const JUMP_VELOCITY = -400.0
 const DECAY = 0.1
 const push_force = 80
 var attack_initiated = false
+const DAMAGE := 50
+const KNOCKBACK := 100
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var state_machine := animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
-func update_animation(directionH, directionV):
-	if !attack_initiated && state_machine.get_current_node() != "Attack":
-		if directionH > 0:
-			animated_sprite.flip_h = false
-		elif directionH < 0:
-			animated_sprite.flip_h = true
-
-	if attack_initiated || state_machine.get_current_node() == "Attack":
-		attack_initiated = false
+func update_animation(directionH, directionV):		
+	if attack_initiated:
 		return
-
+		
 	if directionH == 0 and directionV == 0:
 		animation_tree.get("parameters/playback").travel("Idle")
 	else:
 		animation_tree.get("parameters/playback").travel("Run")
+		run_handler(Vector2(directionH, 0))
 
-#func _ready():
-	#animation_player.connect("animation_finished", _on_animation_finished)
-#
-#func _on_animation_finished(anim_name):
-	#if anim_name == "Attack":
-		#attacking = false
+func run_handler(anim_vector: Vector2) -> void:
+	animation_tree.set("parameters/Run/BlendSpace2D/blend_position", anim_vector.normalized())
+	state_machine.travel("Run")
 		
 func attack_handler() -> void:
 	if Input.is_action_pressed("attack_right"):
 		perform_attack(Vector2(1, 0))
-		animated_sprite.flip_h = false
 	elif Input.is_action_pressed("attack_left"):
 		perform_attack(Vector2(-1, 0))
-		animated_sprite.flip_h = true
 	elif Input.is_action_pressed("attack_up"):
 		perform_attack(Vector2(0, 1))
 	elif Input.is_action_pressed("attack_down"):
@@ -78,9 +69,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= velocity.y * DECAY
 		
 	attack_handler()
-	
-		
+
 	update_animation(directionH, directionV)
+	
 	move_and_slide()
 	
 	for i in get_slide_collision_count():
@@ -92,3 +83,8 @@ func _physics_process(delta: float) -> void:
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if "attack" in anim_name:
 		attack_initiated = false
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(DAMAGE, KNOCKBACK)
